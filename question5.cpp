57a8161607824628e612897539291a44b59e8d10
Faheem-Akram123 <fa1712119@gmail.com>
2026-05-09 09:00:08 +0500

    add q5 using recursion

---
+++ question5.cpp
@@ -0,0 +1,13 @@
+#include "../DSA-headerFiles-using-Cpp/myLL.h"
+
+int main()
+{
+    myLL obj;
+    obj.insertAtTail(1);
+    obj.insertAtTail(2);
+    obj.insertAtTail(3);
+    obj.insertAtTail(4);
+    obj.insertAtTail(5);
+
+    obj.displayR();
+}
